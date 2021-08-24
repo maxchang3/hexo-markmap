ce2f3279101ae6a4e32931038c7576e2a113f57f
MaxChang3 <maxcfoo@gmail.com>
2021-08-24 19:09:33 +0800

    🦄 refactor(Rebuild the main lib):remove markdown-it

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-'use strict';
-const creator = require('./lib/creator');
-const fs = require('hexo-fs');
-const path = require('path');
+const  { transfer } = require("./lib/transfer.js");
 
-hexo.extend.generator.register('markmap_asset', ()=>[
-    {
-      path: 'css/markmap.css',
-      data: function(){
-        return fs.createReadStream(
-          path.resolve(path.resolve(__dirname, "./css/"),"markmap.css"));
-      }
-    }
-]);
 
 hexo.extend.tag.register("markmap", function (args, content) {
-  //console.log(args[0]);
-  var svg = creator(
-    "\`\`\`mindmap\n" + content + "\n\`\`\`"
-  );
-  return `<div class="markmap-container" style="height:`+args[0]+`;" >`+ svg + `</div>`;
+  let code = transfer(`${content.trim()}`);
+  let html = `<div class="markmap-container" style="height:${args[0]}">
+                <svg class="markmap-svg">${code.svgjson}</svg>
+              </div>`
+  return html;
 }, { ends: true });
 
 
 hexo.extend.injector.register('body_end', () => {
-  return `<script src="https://cdn.jsdelivr.net/npm/d3@5"></script>
+  return `
+  <script src="https://cdn.jsdelivr.net/npm/d3@5"></script>
   <script src="https://cdn.jsdelivr.net/npm/markmap-lib@0.7.4/dist/browser/view.min.js"></script>
   <script>
-      const mindmaps = document.querySelectorAll('.markmap-svg');
-   
-      for(const mindmap of mindmaps) {
-          markmap.markmap(mindmap, JSON.parse(mindmap.innerHTML));
-      }
+    const mindmaps = document.querySelectorAll('.markmap-svg');
+    for(let mindmap of mindmaps) {
+        markmap.markmap(mindmap, JSON.parse(mindmap.innerHTML));
+    }
   </script> `
 }, 'post');
 
 hexo.extend.filter.register('after_post_render', (data) => {
-  let my_css = '<link rel="stylesheet" href="/css/markmap.css" type="text/css">';
-  data.content += my_css;
+  let css = `<style>.markmap-container{display:flex;justify-content:center;margin:0 auto;width:90%;height:500px}.markmap-container svg{width:100%;height:100%}@media(max-width:768px){.markmap-container{height:400px}}</style>`;
+  data.content += css;
   return data;
 });
