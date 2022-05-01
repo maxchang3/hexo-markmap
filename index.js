ee6b7ed08b36be9d4f66b964ee0a938786bafba4
MaxChang3 <maxcfoo@gmail.com>
2022-05-01 22:39:33 +0800

    fix(index): lock version of markmap-view to 0.2.7

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ hexo.extend.filter.register('after_post_render', (data) => {
   data.content += `
     <style>.markmap-container{display:flex;justify-content:center;margin:0 auto;width:90%;height:500px}.markmap-container svg{width:100%;height:100%}@media(max-width:768px){.markmap-container{height:400px}}</style>
     <script src="https://cdn.jsdelivr.net/npm/d3@6"></script>
-    <script src="https://cdn.jsdelivr.net/npm/markmap-view"></script>
+    <script src="https://cdn.jsdelivr.net/npm/markmap-view@0.2.7"></script>
     <script> document.querySelectorAll('.markmap-container>svg').forEach(mindmap => markmap.Markmap.create(mindmap, null, JSON.parse(mindmap.getAttribute('data'))))</script>
   `
   return data
