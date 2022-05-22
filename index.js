7cf6877ba6441bad2b328c926f36d27a33998435
MaxChang3 <maxcfoo@gmail.com>
2022-05-22 16:56:20 +0800

    style: format code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { Transformer } = require('markmap-lib');
-const { mainTemplate , containerTemplate , afterRender } = require("./lib/template");
+const { mainTemplate, containerTemplate, afterRender } = require("./lib/template");
 const { fold } = require("./lib/extension");
 const { config } = hexo
 const transformer = new Transformer();
 
-hexo.extend.tag.register("markmap", ([height,depth],markdown) => {
-  const { root:svgData } = transformer.transform(markdown);
-  return containerTemplate(fold(svgData,depth),{height})
+hexo.extend.tag.register("markmap", ([height, depth], markdown) => {
+  const { root: svgData } = transformer.transform(markdown);
+  return containerTemplate(fold(svgData, depth), { height })
 }, { ends: true });
 
-hexo.extend.filter.register('after_render:html', (content) => 
-  afterRender(content,mainTemplate({pjaxEnable:config?.hexo_markmap?.pjax || config?.theme_config?.pjax}))
+hexo.extend.filter.register('after_render:html', (content) =>
+  afterRender(content, mainTemplate({ pjaxEnable: config?.hexo_markmap?.pjax || config?.theme_config?.pjax }))
 )
